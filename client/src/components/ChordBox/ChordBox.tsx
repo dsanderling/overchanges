53d86510167363c6269ec63b091ec076b7414f65
Dave Sanderling <daves@tuta.com>
2026-03-26 16:40:18 -0700

    Add ability to play through chords

--- client/src/components/ChordBox/ChordBox.tsx
+++ client/src/components/ChordBox/ChordBox.tsx
@@ -2,12 +2,14 @@ import type { Chord } from "../../types/chord";
 
 interface ChordBoxProps {
     chord: Chord;
+    isActive: boolean;
     onEdit: () => void;
 }
 
-const ChordBox = ({chord, onEdit }: ChordBoxProps) => {
+const ChordBox = ({chord, isActive, onEdit }: ChordBoxProps) => {
     return (
-        <div onClick={onEdit} style={{ width: `${chord.beats * 48}px`}} className="bg-gray-800 text-white rounded-lg p-3">
+        <div onClick={onEdit} style={{ width: `${chord.beats * 48}px`}} 
+            className={`${isActive ? 'bg-blue-600' : 'bg-gray-800' } text-white rounded-lg p-3`}>
             {chord.root} {chord.quality}
         </div>
     );
--- client/src/components/ChordProgressionEditor/ChordProgressionEditor.tsx
+++ client/src/components/ChordProgressionEditor/ChordProgressionEditor.tsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import type { Chord } from "../../types/chord";
 import ChordBox from "../ChordBox/ChordBox";
 import ChordPicker from "../ChordPicker/ChordPicker";
+import { playTick } from "../../utils/audio";
 
 const ChordProgressionEditor = () => {
     const [chords, setChords] = useState<Chord[]>([]);
     const [editingIndex, setEditingIndex] = useState<number | null>(null);
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+    const [currentBeat, setCurrentBeat] = useState<number>(0);
+    const [bpm, setBpm] = useState<number>(120);
+
+    useEffect(() => { // runs when dependencies change
+        if(!isPlaying) {
+            setCurrentBeat(0);
+            return;
+        }
+        const interval = setInterval(() => { 
+            setCurrentBeat(prev => prev + 1);
+        }, 60000/bpm);
+
+        return () => clearInterval(interval); //cleanup
+    }, [isPlaying, bpm]); // dependencies
+
+    useEffect(() => {
+        if (!isPlaying) return;
+        if (getCurrentChordIndex() === null) {
+            setCurrentBeat(0);
+            setIsPlaying(false);
+        }
+        playTick();
+    }, [currentBeat]);
+
     const handleSelect = (chord: Chord) => {
         if (editingIndex === -1) {
             setChords([...chords, chord]);
@@ -16,14 +42,31 @@ const ChordProgressionEditor = () => {
         setEditingIndex(null);
     }
 
+    const getCurrentChordIndex = () => {
+        let beats = 0;
+        for (let i = 0; i < chords.length; i++) {
+            beats += chords[i].beats;
+            if (currentBeat < beats) return i;
+        }
+        return null; // past the end
+    };
+    const currentChordIndex = getCurrentChordIndex();
+
     return (
         <>
-        <div className=" flex flex-wrap gap-3 p-4 bg-gray-900 rounded-xl min-h-[200px] max-w-2xl">
+        <div className="flex flex-wrap gap-3 p-4 bg-gray-900 rounded-xl min-h-[200px] max-w-2xl">
             {chords.map((chord, index) => (
-                <ChordBox key={index} chord={chord} onEdit={() => setEditingIndex(index)}/>
+                <ChordBox key={index} chord={chord} onEdit={() => setEditingIndex(index)} isActive={index===currentChordIndex}/>
             ))}
             <button onClick={() => setEditingIndex(-1)}>
                 Add chord
+            </button>
+        </div>
+        <div className="flex items-center gap-4 mt-4">
+            <input className="bg-gray-700 text-white rounded p-2 w-20 text-center" type="text" value={bpm} onChange={e => setBpm(Number(e.target.value))} />
+            <label>bpm</label>
+            <button className={`${isPlaying ? 'bg-red-600 hover:bg-red-500' : 'bg-green-600 hover:bg-green-500'} text-white font-bold px-6 py-2 rounded-lg`} onClick={() => setIsPlaying(!isPlaying)}>
+                {isPlaying ? 'Stop' : 'Play'}
             </button>
         </div>
         {editingIndex!==null && (
